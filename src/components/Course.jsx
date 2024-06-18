24f536ca82c5c61dc2bb079f37cfcff78b41c485
[EchoSwift-12] <[amtulrahmansadiya@gmail.com]>
2024-06-18 23:17:58 +0530

    Add props

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,8 +1,9 @@
 
-const Course = () => {
+const Course = ({courseName, courseDescription}) => {
 
-    return <>
-    </>
-  }
-  
+  return <>
+    <h3>{courseName}</h3>
+    <p>{courseDescription}</p>
+  </>
+}
   export default Course
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,8 +1,19 @@
 
+import { useContext } from 'react'
+import { coursesContext } from '../providers/CoursesContextProvider'
+import Course from './Course'
+
 const CourseList = () => {
 
-    return <>
-    </>
+  const { allCourses } = useContext(coursesContext)
+
+  return <>
+  {
+    allCourses.map((course) => {
+    return <Course key={course._id} courseName={course.courseName} courseDescription={course.courseDescription}/>
+    })
   }
+  </>
+}
   
   export default CourseList
